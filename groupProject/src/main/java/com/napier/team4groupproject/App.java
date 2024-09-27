08ea79038cb3f8637f3c2ae12703a54fd7a5e436
Kane Wilkes <40646671@live.napier.ac.uk>
2024-09-27 17:37:03 +0100

    changed the app name from main to app, built app
    Signed-off-by: k4netw <40646671@live.napier.ac.uk>

---
+++ groupProject/src/main/java/com/napier/team4groupproject/App.java
@@ -0,0 +1,9 @@
+package com.napier.team4groupproject;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
--- groupProject/src/main/java/com/napier/team4groupproject/Main.java
+++
@@ -1,7 +0,0 @@
-package com.napier.team4groupproject;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
