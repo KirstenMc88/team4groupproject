606e0c459809f5cd6a4a6b8b04ec8c3e7902c099
klutzeysunshiney <114524764+klutzeysunshiney@users.noreply.github.com>
2024-10-27 11:51:45 +0000

    Created allInWorld method to display all capital cities in world, placeholder code to show method has been created

--- src/main/java/com/napier/team4groupproject/CapitalQueries.java
+++ src/main/java/com/napier/team4groupproject/CapitalQueries.java
@@ -1,4 +1,12 @@
 package com.napier.team4groupproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class CapitalQueries {
+
+    public static void allInWorld() {
+        System.out.println("All Capital Cities in World");
+    }
 }
