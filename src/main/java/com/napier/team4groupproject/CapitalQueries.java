493aaba128ffb08adc9ed3e7c3d938f7f6e33dd6
klutzeysunshiney <114524764+klutzeysunshiney@users.noreply.github.com>
2024-10-27 12:06:37 +0000

    Edited branches to conform with group standard

--- src/main/java/com/napier/team4groupproject/CapitalQueries.java
+++ src/main/java/com/napier/team4groupproject/CapitalQueries.java
@@ -3,10 +3,13 @@ package com.napier.team4groupproject;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.time.LocalDate;
 public class CapitalQueries {
 
     public static void allInWorld() {
+
         System.out.println("All Capital Cities in World");
+        LocalDate today = LocalDate.now();
+        System.out.println("Now is: " + today);
     }
 }
