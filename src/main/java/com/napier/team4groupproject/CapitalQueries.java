4db33b806cdad2988255300237c41cd676cda219
klutzeysunshiney <114524764+klutzeysunshiney@users.noreply.github.com>
2024-10-27 17:53:02 +0000

    Edited allInWorld method which diplays capital city in formation for all capital cities in world sorted by highest population to use standard formatting method App.FormatOutput

--- src/main/java/com/napier/team4groupproject/CapitalQueries.java
+++ src/main/java/com/napier/team4groupproject/CapitalQueries.java
@@ -1,22 +1,20 @@
 package com.napier.team4groupproject;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 
 public class CapitalQueries {
 
-    public static void allInWorld(DatabaseConnection sql) {
+    public static String allInWorld(DatabaseConnection sql) {
 
         System.out.println("All Capital Cities in World");
         LocalDate today = LocalDate.now();
         System.out.println("Now is: " + today);
 
-// SQL query with a placeholder that doesn’t alter the results
+        // SQL query with a placeholder that doesn’t alter the results
         String query = "SELECT city.Name AS Name, country.Name AS Country, city.Population " +
                 "FROM city JOIN country ON city.CountryCode = country.Code " +
                 "WHERE ? = ? " + // Placeholder condition that always evaluates to true
@@ -27,19 +25,15 @@ public class CapitalQueries {
             preparedStatement.setInt(1, 1);
             preparedStatement.setInt(2, 1);
 
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                String cityName = resultSet.getString("Name");
-                String countryName = resultSet.getString("Country");
-                int population = resultSet.getInt("Population");
-
-                System.out.printf("City: %s, Country: %s, Population: %d%n", cityName, countryName, population);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return App.FormatOutput(resultSet);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+
+        }   catch (Exception e) {
+                System.out.println(e.getMessage());
+                return "Error fetching Capital Cities Data";
         }
+
     }
-
-
 }
