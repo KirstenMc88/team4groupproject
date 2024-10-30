55f7a0d09bb78359c3b19ff6cd3781fb3a3d007e
k4netw <40646671@live.napier.ac.uk>
2024-10-30 13:52:57 +0000

    added the populationOfADistrict method with comments, working. also changed some of the comments on recent methods
    Signed-off-by: k4netw <40646671@live.napier.ac.uk>

--- src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
+++ src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
@@ -91,7 +91,7 @@ public class GeneralInfoQueries {
      * Method to query the database, returns the sum of the selected region's population.
      *
      * @param databaseConnection passes in database connection as parameter.
-     * @param region passes in a continent as a parameter.
+     * @param region passes in a region as a parameter.
      * @return returns formatted data by passing the retrieved data through the formatOutput function
      */
 
@@ -133,7 +133,7 @@ public class GeneralInfoQueries {
      * Method to query the database, returns the sum of the selected country's population.
      *
      * @param databaseConnection passes in database connection as parameter.
-     * @param country passes in a continent as a parameter.
+     * @param country passes in a country as a parameter.
      * @return returns formatted data by passing the retrieved data through the formatOutput function
      */
 
@@ -153,6 +153,48 @@ public class GeneralInfoQueries {
 
             // sets the validated country parameter in the query
             preparedStatement.setString(1, validatedCountry);
+
+            // executes the query and stores the result in resultSet
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                // returns formatted result set
+                return App.FormatOutput(resultSet);
+
+            }
+
+        }
+
+        // catches any exceptions which may occur during the query, execution or formatting
+        // then prints the exceptions
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Method to query the database, returns the sum of the selected country's population.
+     *
+     * @param databaseConnection passes in database connection as parameter.
+     * @param district passes in a district as a parameter.
+     * @return returns formatted data by passing the retrieved data through the formatOutput function
+     */
+
+    public static String populationOfADistrict(DatabaseConnection databaseConnection, String district) throws SQLException {
+
+        // header for clarity
+        System.out.println("Population of a country");
+
+        // validates the user input
+        String validatedDistrict = InputValidation.validateStringInput(district);
+
+        // sql query to add all the countries' populations which are part of a specific district
+        String query =  "SELECT SUM(Population) AS Population FROM city WHERE District = ?";
+
+        // prepare the query for execution
+        try(PreparedStatement preparedStatement = databaseConnection.getCon().prepareStatement(query)){
+
+            // sets the validated district parameter in the query
+            preparedStatement.setString(1, validatedDistrict);
 
             // executes the query and stores the result in resultSet
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
