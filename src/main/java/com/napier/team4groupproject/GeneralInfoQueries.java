4dcb940f3021bed7dd90d0896c4933e326eede4f
k4netw <40646671@live.napier.ac.uk>
2024-10-30 13:30:07 +0000

    added the populationOfARegion method with comments, working
    Signed-off-by: k4netw <40646671@live.napier.ac.uk>

--- src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
+++ src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
@@ -87,4 +87,46 @@ public class GeneralInfoQueries {
         }
     }
 
+    /**
+     * Method to query the database, returns the sum of the selected region's population.
+     *
+     * @param databaseConnection passes in database connection as parameter.
+     * @param region passes in a continent as a parameter.
+     * @return returns formatted data by passing the retrieved data through the formatOutput function
+     */
+
+    public static String populationOfARegion(DatabaseConnection databaseConnection, String region) throws SQLException {
+
+        // header for clarity
+        System.out.println("Population of a region");
+
+        // validates the user input
+        String validatedRegion = InputValidation.validateStringInput(region);
+
+        // sql query to add all the countries' populations which are part of a specific region
+        String query =  "SELECT SUM(Population) AS Population FROM country WHERE Region = ?";
+
+        // prepare the query for execution
+        try(PreparedStatement preparedStatement = databaseConnection.getCon().prepareStatement(query)){
+
+            // sets the validated region parameter in the query
+            preparedStatement.setString(1, validatedRegion);
+
+            // executes the query and stores the result in resultSet
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                // returns formatted result set
+                return App.FormatOutput(resultSet);
+
+            }
+
+        }
+
+        // catches any exceptions which may occur during the query, execution or formatting
+        // then prints the exceptions
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
 }
