460e24d6350ace4b178266381b7d5ebe50002283
k4netw <40646671@live.napier.ac.uk>
2024-10-30 12:37:56 +0000

    added populationOfAContinent method, only works if InputValidation.validateStingInput is changed to static, waiting on Rebekka's approval, may need changed. works if so
    Signed-off-by: k4netw <40646671@live.napier.ac.uk>

--- src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
+++ src/main/java/com/napier/team4groupproject/GeneralInfoQueries.java
@@ -45,4 +45,46 @@ public class GeneralInfoQueries {
 
     }
 
+    /**
+     * Method to query the database, returns the sum of the selected continent's population.
+     *
+     * @param databaseConnection passes in database connection as parameter.
+     * @param continent passes in a continent as a parameter.
+     * @return returns formatted data by passing the retrieved data through the formatOutput function
+     */
+
+    public static String populationOfAContinent(DatabaseConnection databaseConnection, String continent) throws SQLException {
+
+        // header for clarity
+        System.out.println("Population of a continent");
+
+        // validates the user input
+        String validatedContinent = InputValidation.validateStringInput(continent);
+
+        // sql query to add all the countries' populations which are part of a specific continent
+        String query =  "SELECT SUM(Population) AS Population FROM country WHERE Continent = ?";
+
+        // prepare the query for execution
+        try(PreparedStatement preparedStatement = databaseConnection.getCon().prepareStatement(query)){
+
+            // sets the validated continent parameter in the query
+            preparedStatement.setString(1, validatedContinent);
+
+            // executes the query and stores the result in resultSet
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                // returns formatted result set
+                return App.FormatOutput(resultSet);
+
+            }
+
+        }
+
+        // catches any exceptions which may occur during the query, execution or formatting
+        // then prints the exceptions
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
 }
