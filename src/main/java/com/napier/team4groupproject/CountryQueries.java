5dd8c53e0d8f986b65b3120f7055002a6e8edf8d
RebekkaEB <rebekkaeb@users.noreply.github.com>
2024-10-30 10:28:08 +0000

    all country queries added

--- src/main/java/com/napier/team4groupproject/CountryQueries.java
+++ src/main/java/com/napier/team4groupproject/CountryQueries.java
@@ -4,43 +4,70 @@ import java.sql.*;
 
 public class CountryQueries {
 
-    /**
-     * Method to get all countries in the world from a database
-     *
-     * <p>This method uses a prepared statement from java.sql to retrieve the information of all countries in the world
-     * from the world database which is connected to the database connection passed into it.<p/>
-     *
-     * @param databaseConnection for access to the world database
-     * @return a string containing the result of the retrieved data, formatted with FormatOutput method from App
-     */
-    public static String CountriesInTheWorld(DatabaseConnection databaseConnection){
-        // header to make it clear what report this is
-        System.out.println("All Countries In The World");
-
-        // sql query to retrieve all countries in the world
-        // displays their country code, name, continent, region, population and capital city
-        // and sorts from biggest population to smallest
+    private static String AllQueries(DatabaseConnection databaseConnection, String attribute, String whereParameter, Integer limitParameter) {
         String query =  "SELECT Code, Name, Continent, Region, Population, " +
                 "(SELECT Name " +
                 "FROM city " +
                 "WHERE ID = country.Capital) " +
                 "AS `Capital`" +
-                "FROM country " +
-                "ORDER BY Population " +
-                "DESC";
-        // try-with-resource for prepared statement
+                "FROM country ";
+
+        if (attribute != null) {
+            query += "WHERE " + attribute + " = ? ";
+        }
+
+        query +=    "ORDER BY Population " +
+                "DESC ";
+
+        if (limitParameter != null) {
+            query += "LIMIT ?";
+        }
+
+        System.out.println(query);
+
         try(PreparedStatement preparedStatement = databaseConnection.getCon().prepareStatement(query)){
 
-            // try-with-resource for result set
+            if (whereParameter != null) {
+                preparedStatement.setString(1, whereParameter);
+                if (limitParameter != null) {
+                    preparedStatement.setInt(2, limitParameter);
+                }
+            } else if (limitParameter != null) {
+                preparedStatement.setInt(1, limitParameter);
+            }
+
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                // return formatted result set
                 return App.FormatOutput(resultSet);
             }
         }
-        // catch exceptions
         catch (Exception e){
             System.out.println(e.getMessage());
             return null;
         }
     }
+
+    public static String CountriesInTheWorld(DatabaseConnection databaseConnection){
+        return AllQueries(databaseConnection, null, null, null);
+    }
+
+    public static String CountriesInAContinent(DatabaseConnection databaseConnection, String continent){
+        return AllQueries(databaseConnection, "Continent", continent, null);
+    }
+
+    public static String CountriesInARegion(DatabaseConnection databaseConnection, String region){
+        return AllQueries(databaseConnection, "Region", region, null);
+    }
+
+    public static String TopCountriesInTheWorld(DatabaseConnection databaseConnection, int limitParameter){
+        return AllQueries(databaseConnection, null, null, limitParameter);
+    }
+
+    public static String TopCountriesInAContinent(DatabaseConnection databaseConnection, String continent, int limitParameter){
+        return AllQueries(databaseConnection, "Continent", continent, limitParameter);
+    }
+
+    public static String TopCountriesInARegion(DatabaseConnection databaseConnection, String region, int limitParameter){
+        return AllQueries(databaseConnection, "Region", region, limitParameter);
+    }
 }
+
