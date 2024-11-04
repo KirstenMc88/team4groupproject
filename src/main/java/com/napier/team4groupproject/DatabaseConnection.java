83d11dcad25afbe6c11935f388eb672b25c0fb47
RebekkaEB <rebekkaeb@users.noreply.github.com>
2024-11-04 19:28:48 +0000

    refactored slightly for unit testing

--- src/main/java/com/napier/team4groupproject/DatabaseConnection.java
+++ src/main/java/com/napier/team4groupproject/DatabaseConnection.java
@@ -11,6 +11,7 @@ public class DatabaseConnection {
      * <p>This field is used by the connect() and disconnect() methods to create and close the connection to the MySQL database.</p>
      */
     private Connection con = null;
+    private String databaseDriver = "com.mysql.cj.jdbc.Driver";
 
     public Connection getCon() {
         return con;
@@ -27,42 +28,50 @@ public class DatabaseConnection {
         try
         {
             // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            int retries;
+            Class.forName(databaseDriver);
+            if (!"UnitTest".equals(System.getProperty("Environment"))) {
+                retries = 10;
+            } else {
+                retries = 3;
+            }
+
+            boolean shouldWait = false;
+            for (int i = 0; i < retries; ++i)
+            {
+                System.out.println("Connecting to database...");
+                try
+                {
+                    if (!"UnitTest".equals(System.getProperty("Environment"))) {
+                        if (shouldWait){
+                            // Wait a bit for db to start
+                            Thread.sleep(delay);
+                        }
+                        // Connect to database
+                        con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                        System.out.println("Successfully connected");
+                        break;
+                    } else {
+                        Thread.sleep(delay);
+                    }
+                }
+                catch (SQLException sqle)
+                {
+                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                    System.err.println("SQLException: " + sqle.getMessage());
+
+                    // Let's wait before attempting to reconnect
+                    shouldWait = true;
+                }
+                catch (InterruptedException ie)
+                {
+                    System.err.println("InterruptedException: " + ie.getMessage());
+                }
+            }
         }
         catch (ClassNotFoundException e)
         {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        boolean shouldWait = false;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                if (shouldWait){
-                    // Wait a bit for db to start
-                    Thread.sleep(delay);
-                }
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-
-                // Let's wait before attempting to reconnect
-                shouldWait = true;
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
+            System.err.println("ClassNotFoundException: " + e.getMessage());
         }
     }
 
@@ -81,9 +90,12 @@ public class DatabaseConnection {
                 // Close connection
                 con.close();
             }
-            catch (Exception e)
+            catch (SQLException e)
             {
-                System.out.println("Error closing connection to database");
+                System.err.println("SQLException: " + e.getMessage());
+            }
+            finally {
+                con = null;
             }
         }
     }
