7453cca22ad3a3ffa506b1edded5d10b657a7023
k4netw <40646671@live.napier.ac.uk>
2024-11-13 13:59:17 +0000

    altered @BeforeAll and added first test. test passes but uses a temporary utility method to change captialQueries from private to static, not final version of the utility method so will need to be changed
    Signed-off-by: k4netw <40646671@live.napier.ac.uk>

--- src/test/java/com/napier/team4groupproject/CapitalQueriesTest.java
+++ src/test/java/com/napier/team4groupproject/CapitalQueriesTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CapitalQueriesTest {
     private static DatabaseConnection worldDB; // db connection with actual data
@@ -19,6 +24,14 @@ public class CapitalQueriesTest {
     private static Integer exampleLimit;
     private static Integer tooHighLimit;
     private static String exampleInvalid;
+
+    // example null inputs
+    private static String nullAttribute = null;
+    private static String nullWhere = null;
+    private static Integer nullLimit = null;
+
+    // reference to the private method
+    private static Method capitalQueriesMethod;
 
     @BeforeAll
     public static void setUp() {
@@ -35,5 +48,28 @@ public class CapitalQueriesTest {
         exampleLimit = 5;
         tooHighLimit = 1000;
         exampleInvalid = "Invalid";
+
+        try {
+            capitalQueriesMethod = Utilities.getAccessibleMethod(
+                    CapitalQueries.class,
+                    "capitalQueries",
+                    DatabaseConnection.class,
+                    String.class,
+                    String.class,
+                    Integer.class
+            );
+        } catch (NoSuchMethodException e) {
+            fail("Failed to access the capitalQueries method: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCapitalQueries_noFiltersNoLimits() {
+        try {
+            String result = (String) capitalQueriesMethod.invoke(null, worldDB, nullAttribute, nullWhere, nullLimit);
+            assertNotNull(result, "The result should not be null for a valid connection with no filters or limits.");
+        } catch (Exception e) {
+            fail("The test should not have thrown an exception" + e.getMessage());
+        }
     }
 }
