eba92ae1c2523158d5345a6231d8502e03621209
RebekkaEB <rebekkaeb@users.noreply.github.com>
2024-11-20 10:55:17 +0000

    refactored to fit naming convention and added comments

--- src/test/java/com/napier/team4groupproject/PopDistributionIntTest.java
+++ src/test/java/com/napier/team4groupproject/PopDistributionIntTest.java
@@ -1,7 +1,6 @@
 package com.napier.team4groupproject;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,7 +9,13 @@ import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PopDistributionTest {
+/**
+ * Integration tests for PopDistributionQueries class
+ *
+ * <p>This class contains integration tests using JUnit to test that all methods in PopDistributionQueries behave
+ * as expected when interacting with several other classes and methods.</p>
+ */
+public class PopDistributionIntTest {
     private static DatabaseConnection worldDB;
     private static DatabaseConnection nullDB;
     private static ByteArrayOutputStream output;
@@ -21,8 +26,16 @@ public class PopDistributionTest {
     private static String exampleCountry;
     private static String exampleWhere;
 
+    /**
+     * Setting up environment
+     *
+     * <p>This method sets up the test environment by setting a System property 'Environment' to 'IntegrationTest'.
+     * This allows certain things in the App class to only be executed in a certain environment.
+     * It also sets up the database connection and different example variables.</p>
+     */
     @BeforeAll
     public static void setUp() {
+        System.setProperty("Environment", "IntegrationTest");
         worldDB = new DatabaseConnection();
         nullDB = new DatabaseConnection();
 
@@ -36,6 +49,16 @@ public class PopDistributionTest {
         exampleCountry = "Germany";
     }
 
+    /**
+     * Method to call the populationDistributionQuery method for different tests
+     * <p>This method calls the populationDistributionQuery method with different parameters and returns the string
+     * which is returned by the populationDistributionQuery method. </p>
+     *
+     * @param sql the DatabaseConnection object used to connect to the world database
+     * @param attribute the column used to filter (e.g., continent, region, country) or null
+     * @param where the user-inputted value for filtering (e.g., "Europe", "Asia") or null
+     * @return formatted string of data containing query results or null if an exception occurs
+     */
     public static String callPopulationDistributionQuery(DatabaseConnection sql, String attribute, String where) {
         try {
             return (String) Utilities.callPrivateMethod(
@@ -51,6 +74,12 @@ public class PopDistributionTest {
     }
 
     // tests with valid parameters
+
+    /**
+     * Testing the populationDistributionQuery method, with correct continent input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_validContinent() {
         try {
@@ -61,6 +90,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the populationDistributionQuery method, with correct region input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_validRegion() {
         try {
@@ -71,6 +105,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the populationDistributionQuery method, with correct country input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_validCountry() {
         try {
@@ -81,6 +120,12 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the populationDistributionQuery method, with null input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null, as it
+     * should return the population of the whole world.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_noFilters() {
         try {
@@ -93,6 +138,11 @@ public class PopDistributionTest {
 
 
     // test for invalid attribute or where
+    /**
+     * Testing the populationDistributionQuery method, with invalid column to filter by
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is null.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_invalidAttribute() {
         try {
@@ -103,6 +153,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the populationDistributionQuery method, with invalid where to filter with
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is null.</p>
+     */
     @Test
     public void testPopulationDistributionQuery_invalidWhere() {
         try {
@@ -113,6 +168,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the ContinentPopulation method, with valid input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testContinentPopulation_notNull() {
         try {
@@ -123,6 +183,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the RegionPopulation method, with valid input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testRegionPopulation_notNull() {
         try {
@@ -133,6 +198,11 @@ public class PopDistributionTest {
         }
     }
 
+    /**
+     * Testing the CountryPopulation method, with valid input
+     *
+     * <p>This test checks that no unexpected exception is thrown, and that the result is not null.</p>
+     */
     @Test
     public void testCountryPopulation_notNull() {
         try {
